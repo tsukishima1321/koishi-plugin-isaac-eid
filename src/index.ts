b88aec1f4fe95457b726e4d81f41eb2a35e0ad9d
tsukishima1321 <18001751830@163.com>
2025-01-13 15:48:41 +0800

    增加初始别名库

--- src/index.ts
+++ src/index.ts
@@ -85,30 +85,35 @@ function pickRandomCollectible(alias: string): number {
   return pool[Math.floor(Math.random() * pool.length)]
 }
 
-async function searchItem(token: string, ctx: Context): Promise<{ [key: itemCode]: itemName}> {
-  let results: { [key: itemCode]: itemName} = {}
+function getItemByItemCode(code: itemCode): item {
+  let type = code[0]
+  let id = code.slice(1)
+  let item = { 'type': 'e', 'id': 9999, 'name': 'error', 'description': 'error' }
+  switch (type) {
+    case 'p':
+      item = pills[parseInt(id)]
+      break
+    case 't':
+      item = trinkets[parseInt(id)]
+      break
+    case 'k':
+      item = cards[parseInt(id)]
+      break
+    case 'c':
+      item = collectibles[parseInt(id)]
+      break
+  }
+  return item
+}
+
+async function searchItem(token: string, ctx: Context): Promise<{ [key: itemCode]: itemName }> {
+  let results: { [key: itemCode]: itemName } = {}
   if (token[0] == 'p' || token[0] == 't' || token[0] == 'k' || token[0] == 'c') {
-    let type = token[0]
-    let id = token.slice(1)
-    let item = { 'type': 'e', 'id': 9999, 'name': 'error', 'description': 'error' }
-    switch (type) {
-      case 'p':
-        item = pills[parseInt(id)]
-        break
-      case 't':
-        item = trinkets[parseInt(id)]
-        break
-      case 'k':
-        item = cards[parseInt(id)]
-        break
-      case 'c':
-        item = collectibles[parseInt(id)]
-        break
-    }
-    if (!item) {
+    let item = getItemByItemCode(token)
+    if (!item || item.name == 'error') {
       return {}
     }
-    results[type + id] = item.name
+    results[item.type + item.id] = item.name
     return results
   }
   const rExp: RegExp = new RegExp(token)
@@ -151,6 +156,47 @@ export function apply(ctx: Context, cfg: Config) {
     alias: 'string'
   }, { autoInc: true })
 
+  ctx.database.get('itemAlias', { itemid: 'k57' }).then((res) => {
+    if (res.length == 0) {
+      ctx.database.upsert('itemAlias', (row) => [
+        { itemid: 'k56', alias: '倒愚者', oriname: '0-愚者？' },
+        { itemid: 'k57', alias: '倒魔术师', oriname: 'I-魔术师？' },
+        { itemid: 'k58', alias: '倒女祭司', oriname: 'II-女祭司？' },
+        { itemid: 'k59', alias: '倒皇后', oriname: 'III-皇后？' },
+        { itemid: 'k60', alias: '倒皇帝', oriname: 'IV-皇帝？' },
+        { itemid: 'k61', alias: '倒教皇', oriname: 'V-教皇？' },
+        { itemid: 'k62', alias: '倒恋人', oriname: 'VI-恋人？' },
+        { itemid: 'k63', alias: '倒战车', oriname: 'VII-战车？' },
+        { itemid: 'k64', alias: '倒正义', oriname: 'VIII-正义？' },
+        { itemid: 'k65', alias: '倒隐者', oriname: 'IX-隐者？' },
+        { itemid: 'k66', alias: '倒命运之轮', oriname: 'X-命运之轮？' },
+        { itemid: 'k67', alias: '倒力量', oriname: 'XI-力量？' },
+        { itemid: 'k68', alias: '倒倒吊人', oriname: 'XII-倒吊人？' },
+        { itemid: 'k69', alias: '倒死亡', oriname: 'XIII-死亡？' },
+        { itemid: 'k70', alias: '倒节制', oriname: 'XIV-节制？' },
+        { itemid: 'k71', alias: '倒恶魔', oriname: 'XV-恶魔？' },
+        { itemid: 'k72', alias: '倒塔', oriname: 'XVI-塔？' },
+        { itemid: 'k73', alias: '倒星星', oriname: 'XVII-星星？' },
+        { itemid: 'k74', alias: '倒月亮', oriname: 'XVIII-月亮？' },
+        { itemid: 'k75', alias: '倒太阳', oriname: 'XIX-太阳？' },
+        { itemid: 'k76', alias: '倒审判', oriname: 'XX-审判？' },
+        { itemid: 'k77', alias: '倒世界', oriname: 'XXI-世界？' },
+        { itemid: 'c105', alias: 'D6', oriname: '六面骰' },
+        { itemid: 'c166', alias: 'D20', oriname: '二十面骰' },
+        { itemid: 'c283', alias: 'D100', oriname: '一百面骰' },
+        { itemid: 'c284', alias: 'D4', oriname: '四面骰' },
+        { itemid: 'c285', alias: 'D10', oriname: '十面骰' },
+        { itemid: 'c386', alias: 'D12', oriname: '十二面骰' },
+        { itemid: 'c406', alias: 'D8', oriname: '八面骰' },
+        { itemid: 'c437', alias: 'D7', oriname: '七面骰' },
+        { itemid: 'c476', alias: 'D1', oriname: '一面骰' },
+        { itemid: 'c489', alias: 'D无限', oriname: '无限面骰' },
+        { itemid: 'c609', alias: '永恒D6', oriname: '永恒六面骰' },
+        { itemid: 'c723', alias: 'D-1', oriname: '计数二十面骰' }
+      ])
+    }
+  })
+
   ctx.command('isaac-eid/eid <arg:string>', '查找以撒物品信息').alias('eid')
     .action(async (_, arg) => {
       if (!arg) {
@@ -159,32 +205,15 @@ export function apply(ctx: Context, cfg: Config) {
       if (arg.length == 0) {
         return '请输入物品名称'
       }
-      let results: { [key: itemCode]: itemName} = await searchItem(arg, ctx)
+      let results: { [key: itemCode]: itemName } = await searchItem(arg, ctx)
       if (Object.keys(results).length == 0) {
         return '未找到物品'
       } else if (Object.keys(results).length == 1) {
-        let result = results[Object.keys(results)[0]]
-        let type = Object.keys(results)[0][0]
-        let id = Object.keys(results)[0].slice(1)
-        let item = { 'type': 'e', 'id': 9999, 'name': 'error', 'description': 'error' }
-        switch (type) {
-          case 'p':
-            item = pills[parseInt(id)]
-            break
-          case 't':
-            item = trinkets[parseInt(id)]
-            break
-          case 'k':
-            item = cards[parseInt(id)]
-            break
-          case 'c':
-            item = collectibles[parseInt(id)]
-            break
-        }
-        if (type == 'c') {
+        let item = getItemByItemCode(Object.keys(results)[0])
+        if (item.type == 'c') {
           return renderCollectible(item, cfg)
         } else {
-          return type + id + " " + item.name + "：\n" + renderDescription(item.description)
+          return item.type + item.id + " " + item.name + "：\n" + renderDescription(item.description)
         }
       } else {
         let resultStr = ''
@@ -227,7 +256,7 @@ export function apply(ctx: Context, cfg: Config) {
       if (!token || !alias) {
         return '请输入物品原名或id和别名'
       }
-      let results: { [key: itemCode]: itemName} = await searchItem(token, ctx)
+      let results: { [key: itemCode]: itemName } = await searchItem(token, ctx)
       if (Object.keys(results).length == 0) {
         return '未找到物品'
       } else if (Object.keys(results).length == 1) {
@@ -250,7 +279,7 @@ export function apply(ctx: Context, cfg: Config) {
       if (!token || !alias) {
         return '请输入物品原名或id和别名'
       }
-      let results: { [key: itemCode]: itemName} = await searchItem(token, ctx)
+      let results: { [key: itemCode]: itemName } = await searchItem(token, ctx)
       if (Object.keys(results).length == 0) {
         return '未找到物品'
       } else if (Object.keys(results).length == 1) {
